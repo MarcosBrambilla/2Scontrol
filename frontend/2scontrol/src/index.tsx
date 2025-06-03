b28e553e149e4f5e6bc8d4c0437afb2040020b43
Marcos Brambilla <marcos.brambilla2003@gmail.com>
2025-06-03 11:28:32 -0300

    initial commits on fronted, starting to create my login screen.

--- frontend/2scontrol/src/index.tsx
+++ frontend/2scontrol/src/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import Login from './pages/Login.tsx';
+import Footer from './components/footer.tsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Login />
+    <Footer />
   </React.StrictMode>
 );
 
