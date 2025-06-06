c2a1e6fd535e4cb9872bd5a726e2fee89f52a56d
Marcos Brambilla <marcos.brambilla2003@gmail.com>
2025-06-06 09:59:33 -0300

    Updating the Users fields to match to the project

--- backend/src/config/syncdb.js
+++ backend/src/config/syncdb.js
@@ -5,14 +5,10 @@ import models from '../models/index.js';
 const syncDB = async () => {
   try{
     await models.user.sync({ alter: true });
-    console.log('The table for the User model was just (re)created!');
     await models.product.sync({ alter: true });
-    console.log('The table for the Product model was just (re)created!');
     await models.supplier.sync({ alter: true });
-    console.log('The table for the Supplier model was just (re)created!');
   } catch (error) {
-    console.error('Unable to Sync database', error)
-  }
-  }
+    console.error('Unable to Sync database', error);
+  }};
 
   export default syncDB;
